d7f4ddc1b26ed839af95e6a8ccff1f0b2910a62d
rachelmiller <rkm2314@tc.columbia.edu>
2017-05-03 18:16:46 -0400

    Added icon, added new images for assignments, change prompt text in xml for assignment maker, started case switch for images (that is not complete)

--- app/src/main/java/com/ba2364/finalproject_benjaminarachelm/AssignmentHolder.java
+++ app/src/main/java/com/ba2364/finalproject_benjaminarachelm/AssignmentHolder.java
@@ -6,6 +6,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class AssignmentHolder extends RecyclerView.ViewHolder {
@@ -16,6 +17,7 @@ public class AssignmentHolder extends RecyclerView.ViewHolder {
     private TextView dueDateText;
     private TextView doneText;
     private Context context;
+    private ImageView assignmentImage;// added to make the image appear with the Assignment
 
     // add image view here for pictures
     public AssignmentHolder(View itemView) {
@@ -25,6 +27,7 @@ public class AssignmentHolder extends RecyclerView.ViewHolder {
         topicNameText = (TextView) itemView.findViewById(R.id.assignment_card_name);
         dueDateText = (TextView) itemView.findViewById(R.id.duedate_card_name);
         doneText = (TextView) itemView.findViewById(R.id.done_card_name);
+        assignmentImage = (ImageView) itemView.findViewById(R.id.image); /// assigned the ImageView
         // assign image view
         this.context = itemView.getContext();
     }
@@ -41,6 +44,33 @@ public class AssignmentHolder extends RecyclerView.ViewHolder {
             doneText.setTextColor(ContextCompat.getColor(context, R.color.notDoneYetColor));
             doneText.setText(R.string.isNotDone);
         }
+
+// this is where we are working on assigning the image
+
+
+        switch (assignment.topic) {
+            case "science":
+                assignmentImage.setImageResource(R.drawable.science_icon);
+            case "english":
+                assignmentImage.setImageResource(R.drawable.english_icon);
+            case "math":
+                assignmentImage.setImageResource(R.drawable.math_icon);
+            case "american history":
+                assignmentImage.setImageResource(R.drawable.americanhistory_icon);
+            case "programming":
+                assignmentImage.setImageResource(R.drawable.programming);
+
+
+                //R.id.maker_delete:
+                //  return true;
+                // default:
+                //   return true;
+        }
+
+
+
+
+
 
         // switch case here and that sets the image based on the name of the assignment
         // need a default image
