7405fef64ee98ee8a966cd3b702096007171ae64
benjaminantell <ba2364@tc.columbia.edu>
2017-05-03 18:44:43 -0400

    FINAL

--- app/src/main/java/com/ba2364/finalproject_benjaminarachelm/AssignmentHolder.java
+++ app/src/main/java/com/ba2364/finalproject_benjaminarachelm/AssignmentHolder.java
@@ -17,9 +17,8 @@ public class AssignmentHolder extends RecyclerView.ViewHolder {
     private TextView dueDateText;
     private TextView doneText;
     private Context context;
-    private ImageView assignmentImage;// added to make the image appear with the Assignment
+    private ImageView assignmentImage;
 
-    // add image view here for pictures
     public AssignmentHolder(View itemView) {
         super(itemView);
         cardView = (CardView) itemView.findViewById(R.id.card_view);
@@ -27,8 +26,7 @@ public class AssignmentHolder extends RecyclerView.ViewHolder {
         topicNameText = (TextView) itemView.findViewById(R.id.assignment_card_name);
         dueDateText = (TextView) itemView.findViewById(R.id.duedate_card_name);
         doneText = (TextView) itemView.findViewById(R.id.done_card_name);
-        assignmentImage = (ImageView) itemView.findViewById(R.id.image); /// assigned the ImageView
-        // assign image view
+        assignmentImage = (ImageView) itemView.findViewById(R.id.image);
         this.context = itemView.getContext();
     }
 
@@ -36,7 +34,7 @@ public class AssignmentHolder extends RecyclerView.ViewHolder {
         assignmentNameText.setText(assignment.topic);
         topicNameText.setText(assignment.yourHomework);
 
-        dueDateText.setText(context.getString(R.string.dueText) + " " +assignment.dateMonth + "/" + assignment.dateDay + "/" + assignment.dateYear);
+        dueDateText.setText(context.getString(R.string.dueText) + " " + assignment.dateMonth + "/" + assignment.dateDay + "/" + assignment.dateYear);
         if (assignment.done) {
             doneText.setTextColor(ContextCompat.getColor(context, R.color.doneColor));
             doneText.setText(R.string.isDone);
@@ -45,35 +43,27 @@ public class AssignmentHolder extends RecyclerView.ViewHolder {
             doneText.setText(R.string.isNotDone);
         }
 
-// this is where we are working on assigning the image
-
-
-        switch (assignment.topic) {
+        switch (assignment.topic.toLowerCase()) {
             case "science":
                 assignmentImage.setImageResource(R.drawable.science_icon);
+                break;
             case "english":
                 assignmentImage.setImageResource(R.drawable.english_icon);
+                break;
             case "math":
                 assignmentImage.setImageResource(R.drawable.math_icon);
+                break;
             case "american history":
                 assignmentImage.setImageResource(R.drawable.americanhistory_icon);
+                break;
             case "programming":
                 assignmentImage.setImageResource(R.drawable.programming);
-
-
-                //R.id.maker_delete:
-                //  return true;
-                // default:
-                //   return true;
+                break;
+            default:
+                assignmentImage.setImageResource(R.drawable.specialassignment_icon);
+                break;
         }
 
-
-
-
-
-
-        // switch case here and that sets the image based on the name of the assignment
-        // need a default image
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
